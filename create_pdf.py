7cb374a18b7124b0268cc913075839c5f23a8fd4
Alexey Bogomolov <mail@abogomolov.com>
2023-12-13 01:00:42 +0300

    add ui

--- create_pdf.py
+++ create_pdf.py
@@ -1,26 +1,54 @@
 import weasyprint
 import os
-
-# Define the HTML content
-html_content = """
-
-
-"""
+from pathlib import Path
+import tkinter as tk
+from tkinter import Text, Scrollbar, Button, Entry
 
 
-def main():
-    # Specify the output file path (desktop in this example)
-
-    home_path = os.getenv("HOMEPATH") or os.getenv("HOME")
-    if not home_path:
-        return
-    output_pdf_path = f"{home_path}/Desktop/analysis.pdf"
-
+def generate_pdf(html_content, output_pdf_path):
     # Generate the PDF
     weasyprint.HTML(string=html_content).write_pdf(output_pdf_path)
 
     print(f"PDF created and saved at {output_pdf_path}")
 
 
+def generate_pdf_gui(html_content, output_pdf_path_var):
+    # Create a Tkinter window for the GUI
+    root = tk.Tk()
+
+    # Create a StringVar for output PDF path
+    output_pdf_path_var = tk.StringVar()
+
+    # Function to generate the PDF
+    def generate_pdf_from_gui():
+        html_content = html_text.get("1.0", tk.END)
+        generate_pdf(html_content, output_pdf_path_var.get())
+        root.quit()  # Close the GUI window after PDF generation
+
+    # Create a Text widget for entering HTML content
+    html_text = Text(root, wrap=tk.WORD, width=60, height=15)
+    html_text.pack(padx=10, pady=10)
+    html_text.insert(tk.END, html_content)  # Pre-fill the Text widget
+
+    # Create a scrollbar for the Text widget
+    scrollbar = Scrollbar(root, command=html_text.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    html_text.config(yscrollcommand=scrollbar.set)
+
+    # Create an entry widget for specifying the output PDF path
+    output_pdf_entry = Entry(root, textvariable=output_pdf_path_var, width=50)
+    output_pdf_entry.pack(pady=10)
+
+    # Pre-fill the path
+    home_path = Path.home().as_posix()
+    output_pdf_path_var.set(f"{home_path}/Desktop/analysis.pdf")
+
+    # Create a button to generate the PDF
+    generate_button = Button(root, text="Generate PDF", command=generate_pdf_from_gui)
+    generate_button.pack(pady=10)
+
+    root.mainloop()
+
+
 if __name__ == "__main__":
-    main()
+    generate_pdf_gui("", "")
